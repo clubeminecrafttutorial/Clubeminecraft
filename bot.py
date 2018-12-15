7fb27eea853175ec2861a58507953ba6e910ccfe
clubeminecrafttutorial <45895477+clubeminecrafttutorial@users.noreply.github.com>
2018-12-15 11:03:25 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def link(ctx):
+    await ctx.send("https://clubeminecraft.com.br/")
+    
+    
+bot.run('NTIzNDU0ODkwMDE4NzM0MTAy.DvZw8g.iAYuYNTDM2XP2n3S2THt8RV3fUw')
